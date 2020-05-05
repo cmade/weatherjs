8aa8a37f9523113ef2a128b926a0098517e3232d
cmade <cmangerere@gmail.com>
2020-05-05 03:28:20 -0400

    Added ui, and local storage.

--- weather.js
+++ weather.js
@@ -1,5 +1,16 @@
 class Weather {
-  constructor() {
-    this.apiKey = apiKey;
+  constructor(city) {
+    this.apiKey = "dd7762140e443b14c84f793f84af7e70";
+    this.city = city;
+  }
+  async getWeather(){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`);
+
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  }
+  changeLocation(city){
+    this.city = city;
   }
 }
